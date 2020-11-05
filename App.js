51629b86f61238493854d7496fda83a425c5624a
Rinon TENDRINOMENA <rinon.ten@onja.org>
2020-11-05 15:11:56 +0300

    Finishing project

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-/**
- * Challenge: build the basic structure of our game
- *
- * 1. <h1> title at the top
- * 2. <textarea> for the box to type in
- *      (tip: React normalizes <textarea /> to be more like <input />,
- *      so it can be used as a self-closing element and uses the `value` property
- *      to set its contents)
- * 3. <h4> ti display the amount of time remaining
- * 4. <button> to start the game
- * 5. Another <h1> to display the word count
- */
+import React, { useEffect, useState } from 'react';
+
+export default function App() {
+    const STARTING_TIME = 0;
+    const [text, setText] = useState("");
+    const [wordCount, setWordCount] = useState(0);
+    const [timeRemaining, setTimeRemaining] = useState(5);
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+    
+    function calculateWordCount(text) {
+        let wordsArr = text.trim().split(" ");
+        setWordCount(wordsArr.filter(word => word !== "").length);
+    }
+
+    function startGame() {
+        setIsTimeRunning(true)
+        setText("") 
+    }
+
+
+    function endGame() {
+        setIsTimeRunning(false)
+        setTimeRemaining(STARTING_TIME)
+        calculateWordCount(text);
+    }
+
+    useEffect(() => {
+        if (isTimeRunning && timeRemaining > 0) {
+             setTimeout(() => {
+                    setTimeRemaining(time => time - 1)}, 1000);
+        } else if (timeRemaining === 0) {
+            endGame()
+        }
+    }, [timeRemaining, isTimeRunning])
+
+   
+
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea value={text} disabled={!isTimeRunning} onChange={(e) => setText(e.target.value)} />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button type="button" disabled={isTimeRunning} onClick={() => startGame(true)}>Start</button>
+            <h4>Word count: {wordCount}</h4>
+        </div>
+    )
+}
